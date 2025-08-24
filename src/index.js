8c006cdec3e17010751511d182403d475a919044
phattran <phattran052004@gmail.com>
2025-08-24 15:32:07 +0700

    delete folder server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const port = 6000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`connect sever successful at http://localhost:${port}`);

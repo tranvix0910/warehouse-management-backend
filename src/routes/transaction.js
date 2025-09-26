eb87bb4819363030d0cc1d19e799c64b686cff83
phattran <phattran052004@gmail.com>
2025-09-26 13:45:53 +0700

    update api delete and update transaction

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createTransaction, getAllTransaction } from '../app/controllers/TransactionController.js';
+import {
+  createTransaction,
+  getAllTransaction,
+  deleteTransaction,
+  updateTransaction,
+} from '../app/controllers/TransactionController.js';
 import { verifyToken } from '../middlewares/verify.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getAllTransaction);
+router.delete('/:id', verifyToken, deleteTransaction);
+router.put('/:id', verifyToken, updateTransaction);
 router.post('/create', verifyToken, createTransaction);
 
 export default router;
